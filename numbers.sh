a9deb656568f3a3578bc59db29a50277376174b3
CCow <44686625+CCow@users.noreply.github.com>
2018-11-17 02:10:32 -0500

    Create numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# numbers.sh
+#Connor Cowher
+echo "Type a positive integer: "
+read NUMBERS
+while echo $NUMBERS | egrep -v "^[USERNAME%2 == 0]" > /dev/null 2>&1
+do
+        echo (NUMBERS + "is odd")
+        NUMBERS -= 1
+done
+echo (NUMBERS + "is even")
+NUMBERS -= 1
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+"numbers.sh" 11L, 231C
